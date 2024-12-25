590ae9ced12f09004a0be61b81e58294dcf9fd02
Luke S <93800822+apotatoa@users.noreply.github.com>
2024-12-25 02:07:44 -0500

    Initial Commit

---
+++ Reverse Scrolling/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Reverse Scrolling
+//
+//  Created by Luke Sbityakov on 12/25/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Reverse ScrollingTests/Reverse_ScrollingTests.swift
@@ -0,0 +1,17 @@
+//
+//  Reverse_ScrollingTests.swift
+//  Reverse ScrollingTests
+//
+//  Created by Luke Sbityakov on 12/25/24.
+//
+
+import Testing
+@testable import Reverse_Scrolling
+
+struct Reverse_ScrollingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
